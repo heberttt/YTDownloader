b1076c60ed8ddbf77c99f7b4c12615ace2971c40
heberttt <hebertp054@gmail.com>
2025-09-17 17:19:50 +0800

    first commit

---
+++ src/main/java/com/hebert/hdownloader/HdownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.hebert.hdownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hebert/hdownloader/HdownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hebert.hdownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
